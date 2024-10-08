2ab8b853eab9d6182f09b3138f4cf0125eb928dd
junseokkim <junsuk8960@naver.com>
2024-10-08 21:43:26 +0900

    feat: create project

---
+++ src/main/java/com/follow_me/running_mate/RunningMateApplication.java
@@ -0,0 +1,13 @@
+package com.follow_me.running_mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/follow_me/running_mate/RunningMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.follow_me.running_mate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
