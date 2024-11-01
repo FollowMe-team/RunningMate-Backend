75bc330a5776a0bcb8b08af9282a7e5325547757
mkshie <87657174+mkshie@users.noreply.github.com>
2024-11-01 22:41:23 +0900

    feat: swagger test 를 위한 코드추가

--- src/main/java/com/follow_me/running_mate/domain/test/controller/S3ImageController.java
+++ src/main/java/com/follow_me/running_mate/domain/test/controller/S3ImageController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,7 +21,7 @@ public class S3ImageController {
 
     private final S3ImageService s3ImageService;
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "이미지 업로드 API", description = "S3에 이미지를 업로드합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "이미지가 업로드되었습니다.",
