ebbb319b13870ba1f613413295068f0fa487b8cd
junseokkim <junsuk8960@naver.com>
2024-11-18 16:05:18 +0900

    feat: cors 에러 해결

--- src/main/java/com/follow_me/running_mate/config/WebConfig.java
+++ src/main/java/com/follow_me/running_mate/config/WebConfig.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public WebMvcRegistrations webMvcRegistrations() {
@@ -20,6 +22,17 @@ public class WebConfig {
                 return new CustomRequestMappingHandlerMapping();
             }
         };
+    }
+
+    // CORS 설정 추가
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowCredentials(true)
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .maxAge(3600);
     }
 
     private static class CustomRequestMappingHandlerMapping extends RequestMappingHandlerMapping {
