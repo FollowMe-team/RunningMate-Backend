75bcca384e9f61a651f6308053c26236145113f4
junseokkim <junsuk8960@naver.com>
2024-11-26 16:07:17 +0900

    feat: 람다 request/response 생성

---
+++ src/main/java/com/follow_me/running_mate/domain/course/dto/response/CourseLambdaResponse.java
@@ -0,0 +1,22 @@
+package com.follow_me.running_mate.domain.course.dto.response;
+
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+public class CourseLambdaResponse {
+    private String difficulty;
+    private String gradient;
+    private List<VoicePointInfo> voicePoints;
+
+    @Getter
+    @Builder
+    public static class VoicePointInfo {
+        private Integer sequenceNumber;
+        private String voiceType;
+    }
+}
